bfa3c90470f188312974d3dbb1069f7a67ad736e
letritrung <letritrung2605@gmail.com>
2025-08-20 08:55:38 +0700

    create project backend

---
+++ src/main/java/com/dreamers/the_dreamers/TheDreamersApplication.java
@@ -0,0 +1,13 @@
+package com.dreamers.the_dreamers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDreamersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDreamersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamers/the_dreamers/TheDreamersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamers.the_dreamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDreamersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
